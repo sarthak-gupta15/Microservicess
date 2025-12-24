0d377cdd486689fb107e7274ce88cab3fd81e66e
sarthak-gupta15 <sarthaksky15@gmail.com>
2025-12-24 08:53:53 +0530

    [ADDED]: Auth Server

--- AuthServer/src/main/java/in/binarybrains/AuthServer/config/Config.java
+++ AuthServer/src/main/java/in/binarybrains/AuthServer/config/Config.java
@@ -2,12 +2,18 @@ package in.binarybrains.AuthServer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,7 +38,7 @@ public class Config {
 
     @Bean
     public SecurityFilterChain securityFilter(HttpSecurity http){
-        http.csrf(csrfCustomizer); // csrf token disable
+//        http.csrf(csrfCustomizer); // csrf token disable
         http.csrf((custom) -> custom.disable() );
         http.authorizeHttpRequests(req -> req.anyRequest().authenticated());
 //        http.formLogin(Customizer.withDefaults()); // browser form
@@ -41,5 +47,33 @@ public class Config {
         SecurityFilterChain securityFilterChain = http.build();
         return  securityFilterChain;
     }
+
+//    userDeatailAuthenctor filter
+    @Bean
+    public UserDetailsService getUserDetailsService(){
+        UserDetails user1 = User.
+                withDefaultPasswordEncoder()
+                .username("pranay")
+                .password("pranay@123")
+                .build();
+        UserDetails user2 = User.
+                withDefaultPasswordEncoder()
+                .username("nikhil")
+                .password("nikhil@123")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+
+    }
+
+    @Bean
+    AuthenticationProvider getAuthProvider(){
+//        Repo = Dao
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(this.getUserDetailsService());
+        return provider;
+    }
+
+
+
 
 }
